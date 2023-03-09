b37c58d74ab04e6d16ee014ebc755668b3751a77
rezagh <rezaghp@gmail.com>
2023-03-09 20:33:04 +1100

    cleanup

--- SerialInput/SerialInput.ino
+++ SerialInput/SerialInput.ino
@@ -1,20 +1,31 @@
+/*
+This is a write and then read from the serial port
+if you write in setup but i do not think it works because the write is done before we go into the read
+**/
+
 int inByte;
 
 void setup(){
   Serial.begin(9600);
-  pinMode(13,OUTPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
   Serial.println("Ready");
 }
 
 void loop(){
+
+  //Serial.write(45);
+
   if(Serial.available() > 0){
     inByte = Serial.read();
     if(inByte =='a'){
-      digitalWrite(13,HIGH);
-      Serial.println("LED ON")
+        digitalWrite(LED_BUILTIN,HIGH);
+        delay(500);
+        Serial.println("LED ON");
     }else{
-      digitalWrite(13,LOW);
-      Serial.println("LED ON")
+      digitalWrite(LED_BUILTIN,LOW);
+      Serial.println("LED OFF");
     }
   }
+
+  delay(5000);
 }
